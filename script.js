ebd0cc9ba444a59178e532ad001654e80b666e5c
2022 MANE <2022mane@Musics-MacBook-Pro.local>
2022-01-16 17:50:13 -0500

    added chacking for length/ too long and short

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ let passwordLength =parseInt( prompt("Please put at least 8 characters and not m
 if(Number.isNaN(passwordLength)){
   alert("bad length. It has to be a number. Please retry.");
   return "bad length. Password not generated. Please retry."
-
-
 }
+if(passwordLength < 8) {
+  alert("password length must be at least 8 characters");
+  return "length less than 8 characters. Password not generated. Please retry."
 }
+
+if(passwordLength > 128) {
+  alert("password length must be less tha 129 characters");
+  return "length more than 128 characters. Password not generated. Please retry."
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
